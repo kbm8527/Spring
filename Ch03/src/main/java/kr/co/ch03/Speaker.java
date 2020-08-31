d8bf9e55705019e94bc41344fc75f95e3e4f2dc2
kbm8527 <kbm8527@gmail.com>
2020-08-31 12:34:17 +0900

    2020/08/31

---
+++ Ch03/src/main/java/kr/co/ch03/Speaker.java
@@ -0,0 +1,15 @@
+package kr.co.ch03;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Speaker  {
+
+	public void soundUp() {
+		System.out.println("Speaker soundUP...");
+	}
+	public void soundDown() {
+		System.out.println("Speaker soundDown...");
+	}
+}
